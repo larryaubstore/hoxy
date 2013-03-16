1bb20c134def282b595288a0f7f07455a0eab0f1
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2013-03-15 21:44:16 -0400

    Empty directory ./plugins/inject created.

--- plugins/inject.js
+++ plugins/inject.js
@@ -16,20 +16,22 @@ exports.run = function(api){
 	if (ct && ct.indexOf('html')>-1 && api.arg.length > 0) {
 
 		var filename = api.arg(0);
-		fs.readFile(filename, function (err, data) {
-		  if (err) throw err;
+		// file located in ./plugins/inject
+		fs.readFile("./plugins/inject/" + filename, function (err, data) {
+		  if (!err) { 
 
-			var html = api.getResponseBody();
-			data = "<script type=\"text/javascript\">" + data + "</script>";
-			var host = api.getRequestInfo().headers.host;
-			if (typeof(html) !== "undefined" && html.length > 0) {
-				html=html.replace(/<body([^>]*)>/, '<body$1>'+ data);
-				api.setResponseBody(html);
-				api.notify();
+				var html = api.getResponseBody();
+				data = "<script type=\"text/javascript\">" + data + "</script>";
+				var host = api.getRequestInfo().headers.host;
+				if (typeof(html) !== "undefined" && html.length > 0) {
+					html=html.replace(/<body([^>]*)>/, '<body$1>'+ data);
+					api.setResponseBody(html);
+				}
 			}
 			else {
-				api.notify();
+				console.log("File not found ! Must be located in ./plugins/inject !");
 			}
+			api.notify();
 		});
 	} else {
 		api.notify();	
