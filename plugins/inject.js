c6c4d2ec48cca2a4414d788ae45cb501849968d5
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2013-03-16 22:19:15 -0400

    Try/catch added in plugin.

--- plugins/inject.js
+++ plugins/inject.js
@@ -16,23 +16,32 @@ exports.run = function(api){
 	if (ct && ct.indexOf('html')>-1 && api.arg.length > 0) {
 
 		var filename = api.arg(0);
-		// file located in ./plugins/inject
-		fs.readFile("./plugins/inject/" + filename, function (err, data) {
-		  if (!err) { 
 
-				var html = api.getResponseBody();
-				data = "<script type=\"text/javascript\">" + data + "</script>";
-				var host = api.getRequestInfo().headers.host;
-				if (typeof(html) !== "undefined" && html.length > 0) {
-					html=html.replace(/<body([^>]*)>/, '<body$1>'+ data);
-					api.setResponseBody(html);
+		try {
+			// file located in ./plugins/inject
+			fs.readFile("./plugins/inject/" + filename, function (err, data) {
+				if (!err) { 
+
+					var html = api.getResponseBody();
+					data = "<script type=\"text/javascript\">" + data + "</script>";
+					var host = api.getRequestInfo().headers.host;
+					if (typeof(html) !== "undefined" && html.length > 0) {
+						html=html.replace(/<body([^>]*)>/, '<body$1>'+ data);
+						api.setResponseBody(html);
+					}
 				}
-			}
-			else {
-				console.log("File not found ! Must be located in ./plugins/inject !");
-			}
+				else {
+					console.log("File not found ! Must be located in ./plugins/inject !");
+				}
+				api.notify();
+			});
+		} catch(err) {
+			console.log(err);
 			api.notify();
-		});
+		}
+
+
+
 	} else {
 		api.notify();	
 	}
